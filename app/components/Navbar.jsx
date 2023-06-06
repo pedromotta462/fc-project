f36642acfbc5f87cb63c15ec48e99dfb60fc794a
pedromotta462 <pedromotta1d2015@gmail.com>
2023-06-06 18:17:08 -0300

    adjusts in navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -6,9 +6,9 @@ import imgs from '../../public/images/index';
 
 const Navbar = ({link}) => {
   return (
-    <div className='z-10 flex items-center justify-between bg-[#f1f1f1] w-[100%] h-[10%] fixed top-0 p-[.5%] shadow-lg'>
+    <div className='z-10 flex items-center justify-between bg-[#f1f1f1] w-[100%] md:h-[12%] xl:h-[7%] fixed top-0 p-[.5%] shadow-lg'>
         <span></span>
-        <Image src={imgs.logoFC} alt="Logo da ferreira costa" className='h-[100%] w-[10%] rounded'>
+        <Image src={imgs.logoFC} alt="Logo da ferreira costa" className='h-[100%] w-[12%] rounded'>
         </Image>
         <div className='navbar'>
             <Link 
