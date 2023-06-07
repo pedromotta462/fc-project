ba7b59507c498cf03f66eb8161a80dee30be9c49
Pedro Motta <pedromotta1d2015@gmail.com>
2023-06-07 02:34:19 -0300

    adjusts in home layout

--- app/Home/layout.js
+++ app/Home/layout.js
@@ -2,18 +2,12 @@ import { Inter } from 'next/font/google'
 import { Input, Button, Navbar, Sidebar } from '../components/components'
 import '../styles/style.css'
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function RootLayout({ children }) {
+export default function HomeLayout({ children }) {
   return (
-    <html lang="pt-br">
-        <body className={inter.className}>
         <main className="h-[100%] flex flex-col items-end justify-center bg-white">
             <Navbar link='/Home/User'/>
             <Sidebar link1='/Home' link2='/Home/ClientInfo' link3='/Home/User' link4='/Home/Configuration' link5='/Home' />
             {children}
           </main>
-        </body>
-    </html>
   )
 }
