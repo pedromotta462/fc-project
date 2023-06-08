19a2e8b6d423910a467dc90025b3d88cdc300e27
pedromotta462 <pedromotta1d2015@gmail.com>
2023-06-07 21:39:10 -0300

    link css in user page for joca use

--- app/Home/User/page.js
+++ app/Home/User/page.js
@@ -1,13 +1,15 @@
-"use client";
-
 import React from "react";
 import { Button } from "../../components/components";
-import GetUsers from "@/app/components/GetUsers";
+import '../../styles/style.css'
 
 const User = () => {
   return (
     <div className="flex min-h-[82vh] w-[94%] flex-col items-center justify-center mt-16 bg-[#f1f1f1] text-black shadow-xl">
-      UserPage
+      
+      <p className="testecss">
+        UserPage
+      </p>
+
     </div>
   );
 };
