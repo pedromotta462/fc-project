55c5f2404885f240ac1aa896e5a35193fe140ab6
pedromotta462 <pedromotta1d2015@gmail.com>
2023-06-08 20:56:07 -0300

    table adjusts

--- app/components/Table.jsx
+++ app/components/Table.jsx
@@ -3,33 +3,52 @@
 import * as React from 'react';
 import { DataGrid, GridToolbar } from '@mui/x-data-grid';
 import { styled } from '@mui/material/styles';
-import { Typography } from '@mui/material';
+import { Tooltip, Typography } from '@mui/material';
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  { field: 'age', headerName: 'Age', type: 'number', width: 90 },
-  { field: 'fullName', headerName: 'Full name', width: 160, valueGetter: (params) => `${params.row.firstName || ''} ${params.row.lastName || ''}` },
-  { field: 'email', headerName: 'Email', width: 200 },
+  { field: 'id', headerName: 'ID', flex: 1 },
+  { field: 'tickets', headerName: 'Tickets do cliente', flex: 2 },
+  { field: 'Resp', headerName: 'Responsável', flex: 1 },
+  { field: 'Number', headerName: 'Número', type: 'number', flex: 1 },
+  { field: 'Status', headerName: 'Status', flex: 1 },
+  { field: 'Subject', headerName: 'Assunto', flex: 2 },
+  { field: 'email', headerName: 'Email', flex: 2 },
 ];
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35, email: 'jon@example.com' },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42, email: 'cersei@example.com' },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45, email: 'jaime@example.com' },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16, email: 'arya@example.com' },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null, email: 'daenerys@example.com' },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150, email: 'melisandre@example.com' },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44, email: 'clifford@example.com' },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36, email: 'frances@example.com' },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65, email: 'roxie@example.com' },
+  { id: 1, Resp: 'Esther', tickets: 'Ticket Atendimento - Acompanhamento de entrega', Number: 35, Status: 'Urgente', Subject: 'Pedido #123456 - Status de entrega e previsão de chegada', email: 'jon@example.com' },
+  { id: 2, Resp: 'Lucas', tickets: 'Ticket Atendimento - Suporte técnico', Number: 42, Status: 'Urgente', Subject: 'Problema com a impressora não reconhecida.', email: 'cersei@example.com' },
+  { id: 3, Resp: 'Pedro', tickets: 'Ticket Atendimento - Troca de produto', Number: 45, Status: 'Urgente', Subject: 'Troca de tamanho de vestuário.', email: 'jaime@example.com' },
+  { id: 4, Resp: 'Bruno', tickets: 'Ticket Atendimento - Acompanhamento de entrega', Number: 16, Status: 'Urgente', Subject: 'Endereço de entrega incorreto, favor retificar', email: 'arya@example.com' },
+  { id: 5, Resp: 'Esther', tickets: 'Ticket Atendimento - Troca de produto', Number: 182, Status: 'Urgente', Subject: 'roduto danificado durante o transporte, necessito de substituição.', email: 'daenerys@example.com' },
+  { id: 6, Resp: 'Bruno', tickets: 'Ticket Atendimento - Suporte técnico', Number: 150, Status: 'Urgente', Subject: 'Erro ao acessar o site', email: 'melisandre@example.com' },
+  { id: 7, Resp: 'Esther', tickets: 'Ticket Atendimento - Acompanhamento de entrega', Number: 44, Status: 'Urgente', Subject: 'Pedido não recebido dentro do prazo estipulado', email: 'clifford@example.com' },
+  { id: 8, Resp: 'Lucas', tickets: 'Ticket Atendimento - Acompanhamento de entrega', Number: 36, Status: 'Urgente', Subject: 'Informações sobre a transportadora responsável pela entrega.', email: 'frances@example.com' },
+  { id: 9, Resp: 'Pedro', tickets: 'Ticket Atendimento - Troca de produto', Number: 65, Status: 'Urgente', Subject: 'Solicitação de troca do produto com defeito', email: 'roxie@example.com' },
+  { id: 10, Resp: 'Esther', tickets: 'Ticket Atendimento - Acompanhamento de entrega', Number: 35, Status: 'Urgente', Subject: 'Pedido #123456 - Status de entrega e previsão de chegada', email: 'jon@example.com' },
+  { id: 11, Resp: 'Lucas', tickets: 'Ticket Atendimento - Suporte técnico', Number: 42, Status: 'Urgente', Subject: 'Problema com a impressora não reconhecida.', email: 'cersei@example.com' },
+  { id: 12, Resp: 'Pedro', tickets: 'Ticket Atendimento - Troca de produto', Number: 45, Status: 'Urgente', Subject: 'Troca de tamanho de vestuário.', email: 'jaime@example.com' },
+  { id: 13, Resp: 'Bruno', tickets: 'Ticket Atendimento - Acompanhamento de entrega', Number: 16, Status: 'Urgente', Subject: 'Endereço de entrega incorreto, favor retificar', email: 'arya@example.com' },
+  { id: 14, Resp: 'Esther', tickets: 'Ticket Atendimento - Troca de produto', Number: 18, Status: 'Urgente', Subject: 'roduto danificado durante o transporte, necessito de substituição.', email: 'daenerys@example.com' },
+  { id: 15, Resp: 'Bruno', tickets: 'Ticket Atendimento - Suporte técnico', Number: 150, Status: 'Urgente', Subject: 'Erro ao acessar o site', email: 'melisandre@example.com' },
+  { id: 16, Resp: 'Esther', tickets: 'Ticket Atendimento - Acompanhamento de entrega', Number: 44, Status: 'Urgente', Subject: 'Pedido não recebido dentro do prazo estipulado', email: 'clifford@example.com' },
+  { id: 17, Resp: 'Lucas', tickets: 'Ticket Atendimento - Acompanhamento de entrega', Number: 36, Status: 'Urgente', Subject: 'Informações sobre a transportadora responsável pela entrega.', email: 'frances@example.com' },
+  { id: 18, Resp: 'Pedro', tickets: 'Ticket Atendimento - Troca de produto', Number: 65, Status: 'Urgente', Subject: 'Solicitação de troca do produto com defeito', email: 'roxie@example.com' },
+  { id: 19, Resp: 'Esther', tickets: 'Ticket Atendimento - Acompanhamento de entrega', Number: 35, Status: 'Urgente', Subject: 'Pedido #123456 - Status de entrega e previsão de chegada', email: 'jon@example.com' },
+  { id: 20, Resp: 'Lucas', tickets: 'Ticket Atendimento - Suporte técnico', Number: 42, Status: 'Urgente', Subject: 'Problema com a impressora não reconhecida.', email: 'cersei@example.com' },
+  { id: 21, Resp: 'Pedro', tickets: 'Ticket Atendimento - Troca de produto', Number: 45, Status: 'Urgente', Subject: 'Troca de tamanho de vestuário.', email: 'jaime@example.com' },
+  { id: 22, Resp: 'Bruno', tickets: 'Ticket Atendimento - Acompanhamento de entrega', Number: 16, Status: 'Urgente', Subject: 'Endereço de entrega incorreto, favor retificar', email: 'arya@example.com' },
+  { id: 23, Resp: 'Esther', tickets: 'Ticket Atendimento - Troca de produto', Number: 120, Status: 'Urgente', Subject: 'roduto danificado durante o transporte, necessito de substituição.', email: 'daenerys@example.com' },
+  { id: 24, Resp: 'Bruno', tickets: 'Ticket Atendimento - Suporte técnico', Number: 150, Status: 'Urgente', Subject: 'Erro ao acessar o site', email: 'melisandre@example.com' },
+  { id: 25, Resp: 'Esther', tickets: 'Ticket Atendimento - Acompanhamento de entrega', Number: 44, Status: 'Urgente', Subject: 'Pedido não recebido dentro do prazo estipulado', email: 'clifford@example.com' },
+  { id: 26, Resp: 'Lucas', tickets: 'Ticket Atendimento - Acompanhamento de entrega', Number: 36, Status: 'Urgente', Subject: 'Informações sobre a transportadora responsável pela entrega.', email: 'frances@example.com' },
+  { id: 27, Resp: 'Pedro', tickets: 'Ticket Atendimento - Troca de produto', Number: 65, Status: 'Urgente', Subject: 'Solicitação de troca do produto com defeito', email: 'roxie@example.com' },
 ];
 
 const DataTableContainer = styled('div')({
   display: 'flex',
   flexDirection: 'column',
-  height: 400,
+  height: 600,
   width: '100%',
   backgroundColor: (theme) => theme.palette.background.paper,
 });
@@ -39,7 +58,21 @@ const Title = styled(Typography)({
   fontWeight: 'bold',
 });
 
+const TooltipCell = styled(Tooltip)`
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
 export default function DataTable() {
+  const renderTooltipCell = (params) => {
+    return (
+      <TooltipCell title={params.value}>
+        <span>{params.value}</span>
+      </TooltipCell>
+    );
+  };
+
   return (
     <DataTableContainer>
       <Title variant="h4" component="div">
@@ -47,8 +80,16 @@ export default function DataTable() {
       </Title>
       <DataGrid
         rows={rows}
-        columns={columns}
-        pageSize={5}
+        columns={columns.map((column) => ({
+          ...column,
+          renderCell: renderTooltipCell,
+        }))}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        pageSizeOptions={[5, 10, 25, 50, 100]}
         checkboxSelection
         components={{
           Toolbar: GridToolbar,
@@ -57,5 +98,6 @@ export default function DataTable() {
     </DataTableContainer>
   );
 }
+
 
 
