a62b9be438861c93438e7945bb8fdace489e3a4a
JocaCavalcanti <joaomcavalcanti.jmcm@gmail.com>
2023-06-08 20:59:25 -0300

    Add firts inputs

--- app/Home/User/page.js
+++ app/Home/User/page.js
@@ -6,9 +6,16 @@ const User = () => {
   return (
     <div className="flex min-h-[82vh] w-[94%] flex-col items-center justify-center mt-16 bg-[#f1f1f1] text-black shadow-xl">
       
-      <p className="testecss">
-        UserPage
-      </p>
+    <main class = "allContent">
+      <div class = "textTop">
+        <p>Principal</p>
+      </div>
+      <div class="inputTop">
+        <p class = "nameAll"><span class="textName">Nome:</span> <input type="text" class="inputName" placeholder="Digite seu nome" /></p>
+        <p class = "nameUser"><span class="textUser">Usuario:</span><input type="text" class="inputUser" placeholder="Digite seu nome" /></p>
+    </div>
+
+    </main>
 
     </div>
   );
