9868bcea486e8a2d3016c0b846b8fc6663718f93
Filipe da Silva Granja <110956203+filipegranja98@users.noreply.github.com>
2023-06-13 01:48:34 -0300

    alterações users

--- app/Home/User/page.js
+++ app/Home/User/page.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Button } from "../../components/components";
 import '../../styles/style.css'
+import { SearchBar } from "../../components/components";
 
 const User = () => {
   return (
     <div className="flex min-h-[82vh] w-[94%] flex-col items-center justify-center mt-16 bg-[#f1f1f1] text-black shadow-xl">
-      
+      <SearchBar/>
     <main class = "allContent">
       <div class = "textTop">
         <h1>Principal</h1>
